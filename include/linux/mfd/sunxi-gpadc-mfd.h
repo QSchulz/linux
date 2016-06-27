ea5393927a03029bf1c108862bd956a2449ceebc
Quentin Schulz <quentin.schulz@free-electrons.com>
2016-06-27 15:41:44 +0200

    mfd: add support for Allwinner SoCs ADC
    The Allwinner SoCs all have an ADC that can also act as a touchscreen
    controller and a thermal sensor. For now, only the ADC and the thermal
    sensor drivers are probed by the MFD, the touchscreen controller support
    will be added later.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@free-electrons.com>

---
+++ include/linux/mfd/sunxi-gpadc-mfd.h
@@ -0,0 +1,23 @@
+/* Header of ADC MFD core driver for sunxi platforms
+ *
+ * Copyright (c) 2016 Quentin Schulz <quentin.schulz@free-electrons>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ */
+
+#ifndef __SUNXI_GPADC_MFD__H__
+#define __SUNXI_GPADC_MFD__H__
+
+#define SUNXI_GPADC_TP_INT_FIFOC            0x10
+#define SUNXI_GPADC_TP_INT_FIFOS            0x14
+
+struct sunxi_gpadc_mfd_dev {
+	struct device			*dev;
+	struct regmap			*regmap;
+	struct regmap_irq_chip_data	*regmap_irqc;
+	void __iomem			*regs;
+};
+
+#endif
