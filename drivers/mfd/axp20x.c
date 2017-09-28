thekeyman/linux-megous

mfd: axp20x: add pinctrl cell for AXP813
As pinctrl and GPIO driver now supports AXP813, add a cell for it.

Signed-off-by: Quentin Schulz <quentin.schulz@free-electrons.com>

drivers/mfd/axp20x.c
@@ -883,6 +883,9 @@ static struct mfd_cell axp813_cells[] = {
 	}, {
 		.name			= "axp20x-battery-power-supply",
 		.of_compatible		= "x-powers,axp813-battery-power-supply",
+	}, {
+		.name			= "axp20x-gpio",
+		.of_compatible		= "x-powers,axp813-pctl",
 	},
 };
 

QSchulz/linux

drivers/mfd/axp20x.c
@@ -876,6 +876,9 @@ static struct mfd_cell axp813_cells[] = {
 		.name			= "axp221-pek",
 		.num_resources		= ARRAY_SIZE(axp803_pek_resources),
 		.resources		= axp803_pek_resources,
+	}, {
+		.name			= "axp20x-gpio",
+		.of_compatible		= "x-powers,axp813-gpio",
 	}
 };
 